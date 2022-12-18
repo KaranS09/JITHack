9ddd928ca4c26599e728ba415a0594ef313db0a6
KaranS09 <karan.sathish980@gmail.com>
2022-12-18 05:46:29 +0530

    Modified changes

--- website/routes.py
+++ website/routes.py
@@ -4,14 +4,22 @@ from website.forms import DataForm
 import tensorflow as tf
 import pandas as pd
 
-
+age = -1
+chol = -1
+cp = -1
 @app.route('/')
 @app.route('/home', methods = ["GET","POST"])
 def home_page():
     model = tf.keras.models.load_model("my_model.h5")
+    global age
+    global chol
+    global cp
     result = -1
     if request.method == "POST":
         data_form = dict(request.form)
+        age = int(request.form.get('age'))
+        chol = int(request.form.get('chol'))
+        cp = int(request.form.get('cp'))
         for i in data_form.copy():
             data_form[i] = float(data_form[i])
         df = pd.DataFrame([data_form])
@@ -24,4 +32,15 @@ def home_page():
 
 @app.route('/blog')
 def blog_page():
-    return render_template('here.html')
+    global age
+    global chol
+    global cp
+    age1 = age
+    chol1 = chol
+    cp1 = cp
+
+    return render_template('here.html',age = age1, chol = chol1, cp = cp1)
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
